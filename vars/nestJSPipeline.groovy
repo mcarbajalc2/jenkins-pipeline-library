fb0031000fc3a5e8027f3a03f88f67a59a2d3d17
mcarbajalc2 <42019150+mcarbajalc2@users.noreply.github.com>
2025-07-19 19:11:12 -0500

    fix environment vars

--- vars/nestJSPipeline.groovy
+++ vars/nestJSPipeline.groovy
@@ -3,12 +3,9 @@ def call(Map config) {
         agent any
 
         environment {
-            IMAGE_NAME       = config.imageName
-            REGISTRY         = config.registry ?: 'localhost:5000'
-            HOST_PORT_PROD   = config.hostPortProd ?: '8080'
-            HOST_PORT_DEV    = config.hostPortDev ?: '8081'
-            CONTAINER_PORT   = config.containerPort ?: '3000'
-            DOCKER_NETWORK   = config.dockerNetwork ?: 'infra_default'
+            IMAGE_NAME     = config.imageName
+            REGISTRY       = config.registry ?: 'localhost:5000'
+            DOCKER_NETWORK = config.dockerNetwork ?: 'infra_default'
         }
 
         stages {
@@ -20,13 +17,13 @@ def call(Map config) {
 
                         if (env.BRANCH_NAME == 'main') {
                             env.TAG  = 'production'
-                            env.APP_PORT = "${HOST_PORT_PROD}"
+                            env.APP_PORT = config.hostPortProd ?: '8080'
                             env.SUPABASE_API_PORT = '9012'
                             env.SUPABASE_STUDIO_PORT = '9014'
                             env.DB_PORT = '9016'
                         } else if (env.BRANCH_NAME == 'develop') {
                             env.TAG  = 'develop'
-                            env.APP_PORT = "${HOST_PORT_DEV}"
+                            env.APP_PORT = config.hostPortDev ?: '8081'
                             env.SUPABASE_API_PORT = '9013'
                             env.SUPABASE_STUDIO_PORT = '9015'
                             env.DB_PORT = '9017'
@@ -34,9 +31,9 @@ def call(Map config) {
                             error "Unsupported branch: ${env.BRANCH_NAME}"
                         }
 
-                        env.FULL_IMAGE = "${REGISTRY}/${IMAGE_NAME}:${TAG}"
+                        env.FULL_IMAGE = "${REGISTRY}/${IMAGE_NAME}:${env.TAG}"
 
-                        echo "Deploying: ${FULL_IMAGE} on host port ${env.APP_PORT} -> container port ${CONTAINER_PORT}"
+                        echo "Deploying: ${env.FULL_IMAGE} on host port ${env.APP_PORT} -> container port ${config.containerPort ?: '3000'}"
                     }
                 }
             }
@@ -50,10 +47,10 @@ def call(Map config) {
             stage('Build & push') {
                 steps {
                     sh """
-                    echo "Building Docker image ${FULL_IMAGE}"
-                    docker build -t ${FULL_IMAGE} .
-                    echo "Pushing Docker image ${FULL_IMAGE}"
-                    docker push ${FULL_IMAGE}
+                    echo "Building Docker image ${env.FULL_IMAGE}"
+                    docker build -t ${env.FULL_IMAGE} .
+                    echo "Pushing Docker image ${env.FULL_IMAGE}"
+                    docker push ${env.FULL_IMAGE}
                     """
                 }
             }
@@ -79,9 +76,9 @@ def call(Map config) {
                     script {
                         echo "Stopping and removing previous app container if exists"
                         sh """
-                        if [ \$(docker ps -q -f name=${IMAGE_NAME}-${TAG}) ]; then
-                            docker stop ${IMAGE_NAME}-${TAG}
-                            docker rm ${IMAGE_NAME}-${TAG}
+                        if [ \$(docker ps -q -f name=${IMAGE_NAME}-${env.TAG}) ]; then
+                            docker stop ${IMAGE_NAME}-${env.TAG}
+                            docker rm ${IMAGE_NAME}-${env.TAG}
                         fi
                         """
                     }
